b62767e7d36011f9972dffd9ca8f053496fc8144
Georgi Tsonkov <gtsonkov@abv.bg>
2020-08-20 22:47:47 +0200

    -RealEstate -Models

--- RealEstate/RealEstates.Services/Contracts/IPropertiesService.cs
+++ RealEstate/RealEstates.Services/Contracts/IPropertiesService.cs
@@ -1,9 +1,14 @@
-﻿namespace RealEstates.Services.Contracts
+﻿using RealEstates.Services.Models;
+using System.Collections.Generic;
+
+namespace RealEstates.Services.Contracts
 {
     public interface IPropertiesService
     {
         public void Create(int seize, int floor, int maxFloorCount, string distinct, string propertyType, string buildingType, int year, int price);
 
-        public void 
+        public IEnumerable<PropertyViewModel> Search(int minYear, int maxYear, int minSize, int maxSize);
+
+        public IEnumerable<PropertyViewModel> SearchByPrice(int minPrice, int maxPrice);
     }
 }
---
+++ RealEstate/RealEstates.Services/PropertyService.cs
@@ -0,0 +1,21 @@
+﻿using RealEstates.Data;
+using RealEstates.Services.Contracts;
+using RealEstates.Services.Models;
+using System.Collections.Generic;
+
+namespace RealEstates.Services
+{
+    public class PropertyService : IPropertiesService
+    {
+        private RealEstateDbContext _db;
+
+        public PropertyService(RealEstateDbContext dbContext)
+        {
+            this._db = dbContext;
+        }
+
+        public void Create(int seize, int floor, int maxFloorCount, string distinct, string propertyType, string buildingType, int year, int price) => throw new System.NotImplementedException();
+        public IEnumerable<PropertyViewModel> Search(int minYear, int maxYear, int minSize, int maxSize) => throw new System.NotImplementedException();
+        public IEnumerable<PropertyViewModel> SearchByPrice(int minPrice, int maxPrice) => throw new System.NotImplementedException();
+    }
+}
