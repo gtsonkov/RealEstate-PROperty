fed36b7380b200f5dfab28b9578e8c6ac59768d4
Georgi Tsonkov <gtsonkov@abv.bg>
2020-08-20 22:42:02 +0200

    -RealEstate -Models

---
+++ RealEstate/RealEstates.Services/Contracts/IPropertiesService.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstates.Services.Contracts
+{
+    public interface IPropertiesService
+    {
+        public void Create(int seize, int floor, int maxFloorCount, string distinct, string propertyType, string buildingType, int year, int price);
+
+        public void 
+    }
+}
