42706058721faa3399f68481b313d0cce5168056
Georgi Tsonkov <gtsonkov@abv.bg>
2020-08-21 16:22:57 +0200

    -RealEstate -Models

--- RealEstate/RealEstates.Services/Contracts/IPropertiesService.cs
+++ RealEstate/RealEstates.Services/Contracts/IPropertiesService.cs
@@ -7,6 +7,8 @@ namespace RealEstates.Services.Contracts
     {
         public void Create(int seize, int? floor, int? maxFloorCount, string distinct, string propertyType, string buildingType, int? year, int price);
 
+        public void UpdateTags(int propertyId);
+
         public IEnumerable<PropertyViewModel> Search(int minYear, int maxYear, int minSize, int maxSize);
 
         public IEnumerable<PropertyViewModel> SearchByPrice(int minPrice, int maxPrice);
--- RealEstate/RealEstates.Services/PropertyService.cs
+++ RealEstate/RealEstates.Services/PropertyService.cs
@@ -3,6 +3,7 @@ using RealEstates.Models;
 using RealEstates.Services.Contracts;
 using RealEstates.Services.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RealEstates.Services
 {
@@ -40,10 +41,66 @@ namespace RealEstates.Services
             {
                 property.TotalNumberOfFloors = null;
             }
+
+
+            #region District
+            //Distrinct
+
+            var currDistrict = this._db.Districts
+                                        .FirstOrDefault(x => x.Name.Trim() == distinct.Trim());
+
+            if (currDistrict == null)
+            {
+                currDistrict = new District
+                {
+                    Name = distinct,
+                };
+            }
+
+            property.District = currDistrict;
+            #endregion
+
+            #region BuildingType
+            //Building
+
+            var currBuildingType = this._db.BuildingTypes
+                                            .FirstOrDefault(x => x.Name.Trim() == buildingType.Trim());
+
+            if (currBuildingType == null)
+            {
+                currBuildingType = new BuildingType
+                {
+                    Name = buildingType
+                };
+            }
+
+            property.BuildingType = currBuildingType;
+            #endregion
+
+            #region PropertyType
+            //PropertyTupe
+
+            var currPropertyType = this._db.PropertyTypes
+                                    .FirstOrDefault(x => x.Name.Trim() == propertyType.Trim());
+
+            if (currPropertyType == null)
+            {
+                currPropertyType = new PropertyType
+                {
+                    Name = propertyType
+                };
+            }
+
+            property.PropertyType = currPropertyType;
+            #endregion
+
+            this._db.Add(property);
+            this._db.SaveChanges();
         }
 
         public IEnumerable<PropertyViewModel> Search(int minYear, int maxYear, int minSize, int maxSize) => throw new System.NotImplementedException();
 
         public IEnumerable<PropertyViewModel> SearchByPrice(int minPrice, int maxPrice) => throw new System.NotImplementedException();
+        public void UpdateTags(int propertyId) => throw new System.NotImplementedException();
     }
 }
